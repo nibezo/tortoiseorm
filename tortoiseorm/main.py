379f2ec4efcd9fccb6999902bfed6e2b35df0399
nibezo <nibezo.cs@gmail.com>
2021-09-05 08:29:50 +0300

    build: create new folder

--- tortoiseorm/main.py
+++ tortoiseorm/main.py
@@ -1,11 +1,11 @@
 from tortoise import Tortoise, run_async
 from database.models import People  # folder|file.py import class_name
-import converter
+from tortoiseorm import converter
 
-idList = converter.convert_id('id.txt')  # converter id from id.txt to Python list
+idList = converter.convert_id('../tortoiseorm/id.txt')  # converter id from id.txt to Python list
 
 
-async def run():
+async def id_database():
     await Tortoise.init(db_url="sqlite://:memory:", modules={"models": ["__main__"]})
     await Tortoise.generate_schemas()
 
@@ -18,4 +18,4 @@ async def run():
     print(await People.all().values("id", "vk_id"))
     # >>> [{'id': 1, 'vk_id': '106320748'}, {'id': 2, 'vk_id': '107964572'}...]
 
-run_async(run())
+run_async(id_database())
