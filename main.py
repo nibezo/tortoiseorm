f54472f605559089974a33b678d079b4fa1fddf0
nibezo <nibezo.cs@gmail.com>
2021-09-05 08:04:54 +0300

    feat: from id list write data to the People table and print it

---
+++ main.py
@@ -0,0 +1,20 @@
+from tortoise import Tortoise, run_async
+from database.models import People  # folder|file.py import class_name
+import converter
+
+idList = converter.convert_id('id.txt')  # converter id from id.txt to Python list
+
+
+async def run():
+    await Tortoise.init(db_url="sqlite://:memory:", modules={"models": ["__main__"]})
+    await Tortoise.generate_schemas()
+
+    for i in range(len(idList)):  # add the id to People table
+        await People(vk_id=idList[i]).save()
+
+    print(await People.all().values_list("id", flat=True))
+    # >>> [1, ..., 309]
+    print(await People.all().values("id", "vk_id"))
+    # >>> [{'id': 1, 'vk_id': '106320748'}, {'id': 2, 'vk_id': '107964572'}...]
+
+run_async(run())
